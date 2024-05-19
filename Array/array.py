d751cc18ed86c9aa26febd2f078afe73acba821d
Sebastian Tamayo <sebastian.tamayo@whereversim.de>
2024-05-19 21:27:44 +0200

    added python tipps

---
+++ Array/array.py
@@ -0,0 +1,44 @@
+# Arrays are lists in python can be declared with list() or [...]
+strArray: [str] = list("aloha", "azucar", "zebra", "car")
+
+# Arrays are lists in python can be declared with list() or [...]
+numberArray: [int] = list(2,4,1,7)
+
+"""
+Copy O(n)
+
+Append[1] O(1)
+
+Pop last O(1)
+
+Pop intermediate[2] O(n)
+
+Insert O(n)
+
+Get Item O(1)
+
+Set Item O(1)
+
+Delete Item O(n)
+
+Iteration O(n)
+
+Get Slice O(k)
+
+Del Slice O(n)
+
+Set Slice O(k+n)
+
+Extend[1] O(k)
+
+Sort O(n log n)
+
+Multiply O(nk)
+
+x in s O(n)
+
+min(s), max(s) O(n)
+
+Get Length O(1)
+
+"""
